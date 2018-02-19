7fabb0cd63d8e62bb313210021fcf7a94be899ff
Robson da Silva <robson@robson.fedora>
2018-02-18 22:54:59 -0300

    Starbase app started

---
+++ src/apps/2-starbase/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app">
+        <h3>{{title}}</h3>
+        <button class="btn btn-primary" v-on:click="fetchCharacter()">Test</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data() {
+            return {
+                title: 'Generate your team'
+            }
+        },
+        methods: {
+            fetchCharacter() {
+                fetch('https://swapi.co/api/people', {
+                    method: 'GET'
+                })        
+                .then(response => response.json())
+                .then(json => console.log(json));
+            }
+        }
+    }
+</script>
