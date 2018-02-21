13c4750c05b9cc391ae4fd25ed02c17739dd3709
Robson da Silva <me@rsilva.net>
2018-02-20 23:08:14 -0300

    Starbase router project started

---
+++ src/apps/3-starbase-router/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <div id="app">
+        <h3>Starbase Router</h3>
+        <h4>
+            <router-link class="link" to="/data/people">Characters</router-link> |
+            <router-link class="link" to="/data/planets">Planets</router-link>
+        </h4>
+        <router-view></router-view>
+    </div>
+</template>
---
+++ src/apps/3-starbase-router/src/components/Data.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="">
+        Type: {{type}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                type: this.$route.params.type
+            }
+        },
+        watch: {
+            '$route': 'change'
+        },
+        methods: {
+            change() {
+                this.type = this.$route.params.type;
+            }
+        }
+    }
+</script>
